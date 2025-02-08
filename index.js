5fd57a232948e8678918b304f28df139f56b56ea
Mtp__ <mtp.dev@pm.me>
2025-02-08 11:51:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,13 @@ db.connect(err => {
 
 // Load commands dynamically
 client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+const commandFolders = fs.readdirSync('./commands');
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
 }
 
 // Event handler
@@ -51,49 +54,6 @@ client.on('messageCreate', async message => {
     } catch (error) {
         console.error(error);
         message.reply('There was an error executing that command!');
-    }
-});
-
-// Moderation Commands
-client.commands.set('ban', {
-    name: 'ban',
-    description: 'Ban a user from the server.',
-    async execute(message, args, db) {
-        if (!message.member.permissions.has('BAN_MEMBERS')) return message.reply("You don't have permission to ban members.");
-        const user = message.mentions.users.first();
-        if (!user) return message.reply('Please mention a user to ban.');
-        const member = message.guild.members.cache.get(user.id);
-        if (!member) return message.reply('User not found in this server.');
-
-        try {
-            await member.ban();
-            message.channel.send(`${user.tag} has been banned.`);
-            db.query('INSERT INTO moderation_logs (user_id, action) VALUES (?, ?)', [user.id, 'ban']);
-        } catch (error) {
-            console.error(error);
-            message.reply('Failed to ban the user.');
-        }
-    }
-});
-
-client.commands.set('kick', {
-    name: 'kick',
-    description: 'Kick a user from the server.',
-    async execute(message, args, db) {
-        if (!message.member.permissions.has('KICK_MEMBERS')) return message.reply("You don't have permission to kick members.");
-        const user = message.mentions.users.first();
-        if (!user) return message.reply('Please mention a user to kick.');
-        const member = message.guild.members.cache.get(user.id);
-        if (!member) return message.reply('User not found in this server.');
-
-        try {
-            await member.kick();
-            message.channel.send(`${user.tag} has been kicked.`);
-            db.query('INSERT INTO moderation_logs (user_id, action) VALUES (?, ?)', [user.id, 'kick']);
-        } catch (error) {
-            console.error(error);
-            message.reply('Failed to kick the user.');
-        }
     }
 });
 
